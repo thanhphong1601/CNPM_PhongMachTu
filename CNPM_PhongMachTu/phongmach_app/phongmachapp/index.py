68e43c0495cf307028279e98803fac63374e6edc
thanhphong1601 <108820649+thanhphong1601@users.noreply.github.com>
2024-05-13 23:28:27 +0700

    Add files via upload
    fix models + add html

---
+++ CNPM_PhongMachTu/phongmach_app/phongmachapp/index.py
@@ -0,0 +1,37 @@
+from flask import render_template, request, redirect
+import dao
+from phongmachapp import app
+
+
+@app.route('/')
+def index():
+    categories = dao.load_categories()
+    return render_template('index.html', categories=categories)
+
+
+@app.route('/benhnhan')
+def user():
+    return render_template('trangBenhNhan.html')
+
+
+@app.route('/dangkykham', methods=['get', 'post'])
+def dangKyLich():
+    if request.method.__eq__('POST'):
+        print(request.form)
+    return render_template('dangKyKham.html')
+
+
+@app.route('/login', methods=['get', 'post'])
+def login_my_user():
+    if request.method.__eq__('POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username.__eq__('admin') and password.__eq__('1'):
+            return redirect('/')
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        app.run(debug=True)
