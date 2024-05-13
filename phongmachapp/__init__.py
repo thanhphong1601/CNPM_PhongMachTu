d283bd789015dad13ef9b97f48aa12bb76153a8b
thanhphong1601 <108820649+thanhphong1601@users.noreply.github.com>
2024-05-13 23:26:08 +0700

    Delete phongmachapp directory

--- phongmachapp/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from urllib.parse import quote
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/phongmachdb?charset=utf8mb4" % quote('Megamanzx123')
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-
-db = SQLAlchemy(app)
--- phongmachapp/dao.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def load_categories():
-    with open('data/categories.json', encoding='utf-8') as f:
-        return json.load(f)
-
-if __name__ == '__main__':
-    print(load_categories())
