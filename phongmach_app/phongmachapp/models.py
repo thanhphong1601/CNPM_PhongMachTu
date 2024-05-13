6c07c7bf7717a6f7b93601b85a6b933a203bbeab
VanHuy1712 <148675214+VanHuy1712@users.noreply.github.com>
2024-05-13 15:46:46 +0700

    sua models

--- phongmach_app/phongmachapp/models.py
+++ phongmach_app/phongmachapp/models.py
@@ -20,6 +20,12 @@ class DonViThuoc(RoleEnum):
     Vien = 3
 
 
+class GioiTinh(RoleEnum):
+    Nam = 1
+    Nu = 2
+    KhongTraLoi = 3
+
+
 class Base(db.Model):
     __abstract__ = True
 
@@ -27,44 +33,50 @@ class Base(db.Model):
 
 
 class NguoiDung(Base, UserMixin):
-    hoTen = Column(String(100))
-    anhDaiDien = Column(String(100))
+    hoTen = Column(String(100), nullable=False)
+    anhDaiDien = Column(String(100), nullable=False)
     tenNguoiDung = Column(String(50), unique=True)
-    matKhauNguoiDung = Column(String(50))
-    gioiTinh = Column(String(10))
-    namSinh = Column(DateTime, default=datetime.now())
-    soDienThoai = Column(String(10))
-    diaChi = Column(String(100))
+    matKhauNguoiDung = Column(String(50), nullable=False)
+    gioiTinh = Column(Enum(GioiTinh), default=GioiTinh.Nam)
+    namSinh = Column(DateTime, nullable=False)
+    soDienThoai = Column(String(10), nullable=False)
+    diaChi = Column(String(100), nullable=False)
     vaiTro_NguoiDung = Column(Enum(VaiTroNguoiDung), default=VaiTroNguoiDung.BenhNhan)
+    phieuKham = relationship('PhieuKham', backref='nguoidung', lazy=True)
+    phieuDangKyKham = relationship('PhieuDangKyKham', backref='nguoidung', lazy=True)
+    hoaDon = relationship('HoaDon', backref='nguoidung', lazy=True)
+    # backref dùng để truy vấn ngược lại dễ hơn,
+    # lazy được sử dụng để xác định cách truy xuất dữ liệu từ cơ sở dữ liệu khi cần thiết
     # active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.hoTen
 
     def nam_sinh(self):
-        return self.namSinh.year if self.namSinh else None
+        if self.namSinh:
+            return self.namSinh.year
+        else:
+            None
 
 
 class PhieuKham(Base):
-    nguoiDung_id = Column(Integer, ForeignKey(NguoiDung.id), nullable=False)
+    benhNhan_id = Column(Integer, ForeignKey(NguoiDung.id), nullable=False)
+    hoTen = Column(String(100), nullable=False)
     trieuChung = Column(String(50), nullable=False)
     duDoanBenh = Column(String(50), nullable=False)
     ngayKham = Column(DateTime, default=datetime.now())
-    hoTenNguoiDung = relationship('NguoiDung', backref='phieukham', lazy=True)
-    # backref dùng để truy vấn ngược lại dễ hơn,
-    # lazy được sử dụng để xác định cách truy xuất dữ liệu từ cơ sở dữ liệu khi cần thiết.
+    chiTietPhieuKham = relationship('ChiTietPhieuKham', backref='phieukham', lazy=True)
+    hoaDon = relationship('HoaDon', backref='phieukham', lazy=True)
 
-    def benhnhan_name(self):
-        if self.hoTenNguoiDung.vaiTro_NguoiDung == VaiTroNguoiDung.BenhNhan:
-            return self.hoTenNguoiDung.hoTen
-        else:
-            return None
+    def __str__(self):
+        return self.hoTen
 
 
 class Thuoc(Base):
     tenThuoc = Column(String(50), unique=True, nullable=False)
     congDung = Column(String(50), nullable=False)
     donViThuoc = Column(Enum(DonViThuoc), default=DonViThuoc.Vien)
+    chiTietPhieuKham = relationship('ChiTietPhieuKham', backref='thuoc', lazy=True)
 
     def __str__(self):
         return self.tenThuoc
@@ -85,15 +97,22 @@ class HoaDon(Base):
     phieuKham_id = Column(Integer, ForeignKey(PhieuKham.id), nullable=False)
 
 
+class LichKham(Base):
+    ngayKham = Column(DateTime, default=datetime.now(), nullable=False)
+    danhSachKham = relationship('DanhSachKham', backref='lichkham', lazy=True)
+
+
+class DanhSachKham(Base):
+    benhNhan_id = Column(Integer, ForeignKey(NguoiDung.id), nullable=False)
+    lichNgayKham_id = Column(Integer, ForeignKey(LichKham.id), nullable=False)
+
+
 class PhieuDangKyKham(Base):
     benhNhan_id = Column(Integer, ForeignKey(NguoiDung.id), nullable=False)
     yTa_id = Column(Integer, ForeignKey(NguoiDung.id), nullable=False)
-    NguoiDung = relationship('NguoiDung', backref='phieudangkykham', lazy=True)
 
-    def benhnhan_id(self):
-        if (self.NguoiDung.vaiTro_NguoiDung == VaiTroNguoiDung.BenhNhan
-                and self.NguoiDung.vaiTro_NguoiDung == VaiTroNguoiDung.YTa):
-            return self.benhNhan_id, self.yTa_id
-        else:
-            return None
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
 
