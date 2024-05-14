9dfe130584b7796be0af0d8953a2a2c9f7f73a65
thanhphong1601 <108820649+thanhphong1601@users.noreply.github.com>
2024-05-14 22:50:04 +0700

    Add files via upload
    fix models + add login + admin site

--- phongmach_app/phongmachapp/dao.py
+++ phongmach_app/phongmachapp/dao.py
@@ -1,9 +1,22 @@
-import json
+import json, hashlib
+from phongmachapp.models import Thuoc, LoaiThuoc, DonViThuoc, NguoiDung
 
 
 def load_categories():
     with open('data/categories.json', encoding='utf-8') as f:
         return json.load(f)
+
+
+def get_user_by_id(id):
+    return NguoiDung.query.get(id)
+
+
+def auth_user(username, password):
+    password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+    print(password)
+    print(username)
+    return NguoiDung.query.filter(NguoiDung.username.__eq__(username.strip()),
+                                  NguoiDung.password.__eq__(password)).first()
 
 
 if __name__ == '__main__':
--- phongmach_app/phongmachapp/index.py
+++ phongmach_app/phongmachapp/index.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect
 import dao
-from phongmachapp import app, admin
+from phongmachapp import app, admin, login
+from flask_login import login_user, current_user, logout_user
 
 
 @app.route('/')
@@ -23,13 +24,49 @@ def dangKyLich():
 
 @app.route('/login', methods=['get', 'post'])
 def login_my_user():
+    if current_user.is_authenticated:
+        return redirect('/')
+    err_msg = ''
     if request.method.__eq__('POST'):
         username = request.form.get('username')
         password = request.form.get('password')
 
-        if username.__eq__('admin') and password.__eq__('1'):
+        user = dao.auth_user(username=username, password=password)
+        if user:
+            login_user(user)
             return redirect('/')
-    return render_template('login.html')
+        else:
+            err_msg = 'Tài khoản hoặc mật khẩu không đúng'
+    return render_template('login.html', err_msg=err_msg)
+
+
+@app.route('/logout', methods=['get'])
+def logout_my_user():
+    logout_user()
+    return redirect('/login')
+
+
+@app.route('/register')
+def register_user():
+    return render_template('/register.html')
+
+
+@app.route('/admin-login', methods=['post'])
+def process_admin_login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    # print(username)
+    # print(password)
+    u = dao.auth_user(username=username, password=password)
+    if u:
+        login_user(user=u)
+
+    return redirect('/admin')
+
+
+@login.user_loader
+def load_user(user_id):
+    return dao.get_user_by_id(user_id)
 
 
 if __name__ == '__main__':
